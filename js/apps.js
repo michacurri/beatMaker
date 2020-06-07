334b186d12de8d10988cb76ffd269a8a7a028867
michacurri <mkcurri@gmail.com>
2020-06-07 17:28:28 -0400

    cleaned up variable naming; added a function to capture keyCode and timeStamp information

--- js/apps.js
+++ js/apps.js
@@ -1,81 +1,59 @@
-// beatMaker
-
-// playable on keyboard using data-key keybindings: LeftHand [E,R,D,F] and RightHand [U,I,J,K].
-// or on mobile using thumbs 
-
-// jQuery will use on clicks, or keydown, event listeners for user interaction.
-
-// sounds will be provided either in an assets folder and initiated using the <audio> tag and JQuery .keydown(Handler)
-// or be integrated using a library such as Howler API.
-
-// each pad will (for MVP) be linked to a single sound.
-
-
-// ** STRETCH TARGET ** //
-
-// Stretch target will allow users to record for a period of time and layer their sounds via a loop
-// the MediaRecorder API may be used in this feature and information stored in an object array using separate variables. 
-// Set Timeout, Set Interval API's may be used to store timing information.
-
-
-
-//// my code
-
 // global variables
-
 const userRecord = [];
 const pads = $('.pad');
+// const audioKeyCodes = [69,82,85,73,68,70,74,75]
 
-///////////// do not delete
-
-// record each keydown and put the keyCode into userRecord Array
-// record time of each keydown with event.timeStamp
-
-// const timeStamp = event.timeStamp;
-// userRecord.push({
-//   keyCode,
-//   timeStamp
-// });
-
-// find the time difference between
-
-////////////////// do not delete
-
+const getTimeStamp = ((keyCode, timeStamp) => {
+  console.log(`${keyCode} and ${timeStamp}`)
+  userRecord.push({
+    keyCode,
+    timeStamp
+  });
+});
 
 // FUNCTIONS
 function clickFunction(e) {
-  const dataKey = e.currentTarget.attributes["data-key"];
-  const clickAudio = $(`audio[data-key="${dataKey.nodeValue}"]`);
-  const clickPad = $(`.pad[data-key="${dataKey.nodeValue}"]`);
-  playAudio(clickAudio);
-  addClass(clickPad);
-  removeClass(clickPad);
+  const timeStamp = e.timeStamp;
+  const keyCode = e.currentTarget.attributes["data-key"].value;
+  const audio = $(`audio[data-key="${keyCode}"]`);
+  const pad = $(`.pad[data-key="${keyCode}"]`);
+  playAudio(audio);
+  addClass(pad);
+  removeClass(pad);
+  getTimeStamp(keyCode, timeStamp);
 };
 
 function keyFunction(e) {
+  const timeStamp = e.timeStamp;
   //which key has been hit
   const keyCode = e.which;
   //find the corresponding audio file linked to the keydown 
   const audio = $(`audio[data-key="${keyCode}"]`);
   //pad also labeled with keyCode, find the correct pad and activate
   const pad = $(`.pad[data-key="${keyCode}"]`);
-
-  // create if keys are data-keys from this array, playAudio(), else
   playAudio(audio);
   addClass(pad);
-  removeClass(pad); 
+  removeClass(pad);
+  getTimeStamp(keyCode, timeStamp)
 }
 
+
+// to play audio files and to allow repeated key presses at user discretion
 function playAudio(audio) {
+  // to do: create "if pressed keys are data-keys from this array", playAudio(), else
+  // ... to allow other keys to be bound (record, bpm, sound, help)
   if (!audio) return;
   audio.get(0).currentTime = 0;
   audio.get(0).play();
 };
 
+// change styling effects for pad interaction
 function addClass(onPad) {
   onPad.addClass('padActive');
 };
 
+// to remove the above mentioned styling after the CSS transition has ended
+// thank you to Talia for the help with my $.each loop on transitionend
 function removeClass() {
   $.each(pads, (index, offPad) => {
     $(offPad).on('transitionend', function () {
@@ -84,49 +62,43 @@ function removeClass() {
   });
 };
 
+// instructions: open and close window
 function openHelp() {
-  $('#openHelpBtn').on('click', function() {
+  $('#openHelpBtn').on('click', function () {
     $('.help').addClass('openHelp');
   });
 };
 
 function closeHelp() {
-  $('#closeHelpBtn').on('click', function() {
+  $('#closeHelpBtn').on('click', function () {
     $('.help').removeClass('openHelp');
   });
 };
 
 
-
-
 // INITIALIZE
 function init() {
 
-  // flash the border of the power button to signify it needs to be turned on
+  // power button only visible object to signify it needs to be turned pushed
   $('#power').on('click', function () {
     $('.switch').toggleClass('switchOn');
     $('.pad').toggleClass('padOn');
-
   })
-  // once pressed, state of machine (pads, other buttons) will light up
 
-  // instructions pop up
+  // functionality for click (mobile or mouse experience)
 
-
-
-  // on click (mobile or mouse experience)
   $('.pad').on('click', function (e) {
     clickFunction(e);
-    console.log();
-
   })
 
+
+  // functionality for keyboard interaction
   $('body').on('keydown', function (e) {
     keyFunction(e);
   })
-
 }
 
+// ** some functionality designed around a javascript drum machine created by Wes Bos: https://www.youtube.com/watch?v=VuN8qwZoego
 
 $(function () {
   init();
